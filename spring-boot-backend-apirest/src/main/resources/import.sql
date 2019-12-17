a518cc08b1e0da8b501c4375c2897786a65b03cf
Anthony Carrasco <thony10.rc.c@gmail.com>
2019-12-16 23:19:56 -0500

    AC: subiendo cambios correctos

--- spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -1,23 +1,30 @@
-/* Populate tabla clientes */
 
-INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamérica');
-INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamérica');
-INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamérica');
-INSERT INTO regiones (id, nombre) VALUES (4, 'Europa');
-INSERT INTO regiones (id, nombre) VALUES (5, 'Asia');
-INSERT INTO regiones (id, nombre) VALUES (6, 'Africa');
-INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
-INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida');
 
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1, 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(2, 'Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(4, 'Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(3, 'Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(5, 'Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(6, 'Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
-INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(7, 'Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
+INSERT INTO regiones (nombre) VALUES('Perú');
+INSERT INTO regiones (nombre) VALUES('Colombia');
+INSERT INTO regiones (nombre) VALUES('Brasil');
+INSERT INTO regiones (nombre) VALUES('Canada');
+INSERT INTO regiones (nombre) VALUES('Chile');
+INSERT INTO regiones (nombre) VALUES('Argentina');
+INSERT INTO regiones (nombre) VALUES('Bolivia');
+
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'thony', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(2,'gre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(3,'fre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(2,'ru', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(3,'mar', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(2,'hugo', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'thony', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'gre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'fre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(3,'ru', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'mar', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(2,'hugo', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'thony', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(3,'gre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'fre', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(4,'ru', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'mar', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(2,'hugo', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'thony', 'carrascp', 'thony@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'gre', 'carrascp', 'thony@gmail.com', '2018-01-01');
--- spring-boot-backend-apirest/src/test/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/SpringBootBackendApirestApplicationTests.java
+++ spring-boot-backend-apirest/src/test/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/SpringBootBackendApirestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.springboot.backend.apirest;
+package com.thony.spring.boot.backend.apirest.springbootbackendapirest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootBackendApirestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
+
